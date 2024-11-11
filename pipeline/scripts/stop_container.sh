82af6155db39c83ecd67b1ada75e839cec7923e2
Raghad Salem <raghadnadersalem20022017@gmail.com>
2024-11-11 18:10:08 +0200

    fix stop container script by removing mongo container

--- pipeline/scripts/stop_container.sh
+++ pipeline/scripts/stop_container.sh
@@ -8,8 +8,8 @@ sudo usermod -aG docker ec2-user
 echo "test-api is running, stop and delete 📛 ..."
 sudo docker stop test-api || true
 sudo docker rm -f test-api || true
-# sudo docker stop mongo || true
-# sudo docker rm -f mongo || true
+sudo docker stop mongo || true
+sudo docker rm -f mongo || true
 
 echo "removes unused Docker images from your system 📛 ..."
 sudo docker image prune --filter "dangling=true" -f || true
